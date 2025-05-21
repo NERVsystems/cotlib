96e9068939ef496734ce7a190aecf89f89852ede
P. D. Finn KD9WEH <pdfinn@users.noreply.github.com>
2025-05-22 02:12:26 +0700

    test(benchmarks): add ValidateType and catalog benchmarks

--- cotlib_bench_test.go
+++ cotlib_bench_test.go
@@ -66,6 +66,39 @@ func BenchmarkUnmarshalXMLEventNoPool(b *testing.B) {
 	}
 }
 
+func BenchmarkValidateType(b *testing.B) {
+	types := []string{
+		"a-f-G",       // Valid basic type
+		"a-f-G-E-X-N", // Valid catalog type
+		"a-f-G-*",     // Valid wildcard
+		"a-.-X",       // Valid atomic wildcard
+		"invalid",     // Invalid
+		"a-f-INVALID", // Invalid format
+		"a-f-G-Z-Z-Z", // Unknown but valid format
+	}
+	b.ReportAllocs()
+	for _, typ := range types {
+		b.Run(typ, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = ValidateType(typ)
+			}
+		})
+	}
+}
+
+func BenchmarkDecodeWithLimits(b *testing.B) {
+	xmlStr := `<event version="2.0" uid="bench" type="a-f-G"><point lat="1" lon="2" hae="3"/></event>`
+	data := []byte(xmlStr)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		dec := xml.NewDecoder(bytes.NewReader(data))
+		evt := Event{}
+		if err := decodeWithLimits(dec, &evt); err != nil {
+			b.Fatalf("decodeWithLimits error: %v", err)
+		}
+	}
+}
+
 func unmarshalXMLEventNoPool(data []byte) (*Event, error) {
 	if doctypePattern.Match(data) {
 		return nil, ErrInvalidInput
